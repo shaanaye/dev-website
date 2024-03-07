d6765fcbe9eec9cc3ed3545c9194bb806a1f5780
shaanaye <72814715+shaanaye@users.noreply.github.com>
2024-03-07 12:22:57 -0500

    messin about

--- main.js
+++ main.js
@@ -1,24 +1,73 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+const scene = new THREE.Scene();
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-setupCounter(document.querySelector('#counter'))
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+// const bookGeometry = new THREE.BoxGeometry(10, 15, 4);
+// const bookMaterial = new THREE.MeshStandardMaterial({color: 0xFFFFFF});
+// const book = new THREE.Mesh(bookGeometry, bookMaterial);
+// scene.add(book);
+
+const ambientLight = new THREE.AmbientLight( 0xffffff );
+const pointLight = new THREE.PointLight(0xffffff);
+ambientLight.position.set(25,25,25);
+pointLight.position.set(25,25,25);
+scene.add(ambientLight, pointLight);
+
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar(){
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+const mo = new THREE.TextureLoader().load('mo.PNG');
+scene.background = mo;
+
+const ammarTexture = new THREE.TextureLoader().load('gang.PNG');
+const ammar = new THREE.Mesh(
+  new THREE.SphereGeometry(5,5,5),
+  new THREE.MeshBasicMaterial({map: ammarTexture})
+);
+
+scene.add(ammar);
+
+Array(200).fill().forEach(addStar);
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  // book.rotation.x += 0.01;
+  // book.rotation.y += 0.05;
+  // book.rotation.z += 0.01;
+  ammar.rotation.x += 0.01;
+  ammar.rotation.y += 0.05;
+  ammar.rotation.z += 0.01;
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
